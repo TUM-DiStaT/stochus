3d9a45b52cc6b07bbfa1ba678b9c2e9e10898966
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-02-22 22:46:46 +0100

    Create library for daisy-ui components

---
+++ libs/daisy-ui/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/button/alert.component'
---
+++ libs/daisy-ui/src/lib/button/alert.component.ts
@@ -0,0 +1,31 @@
+import { Component, HostBinding, Input } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import * as classNames from 'classnames'
+
+export enum AlertType {
+  INFO = 'info',
+  SUCCESS = 'success',
+  WARNING = 'warning',
+  ERROR = 'error',
+}
+
+@Component({
+  selector: 'daisy-alert',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './alert.component.html',
+})
+export class AlertComponent {
+  @Input()
+  type = AlertType.INFO
+
+  @HostBinding('class')
+  get className() {
+    return classNames('alert shadow-lg', {
+      'alert-info': this.type === AlertType.INFO,
+      'alert-success': this.type === AlertType.SUCCESS,
+      'alert-warning': this.type === AlertType.WARNING,
+      'alert-error': this.type === AlertType.ERROR,
+    })
+  }
+}
