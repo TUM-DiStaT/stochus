ec200b4365ec60c13153aee046f6b7b273330549
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-04-09 22:28:40 +0200

    Fix standard toast alert tests

--- libs/daisy-ui/src/lib/toast/standard-toast-alert/standard-toast-alert.component.spec.ts
+++ libs/daisy-ui/src/lib/toast/standard-toast-alert/standard-toast-alert.component.spec.ts
@@ -1,9 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { StandardToastAlertComponent } from './standard-toast-alert.component'
+import { AlertType } from '../../alert/alert.component'
+import {
+  ToastHorizontalAlignment,
+  ToastVerticalAlignment,
+} from '../toast.component'
 
 describe('StandardToastAlertComponent', () => {
   let component: StandardToastAlertComponent
   let fixture: ComponentFixture<StandardToastAlertComponent>
+
+  const exampleToast = {
+    type: AlertType.ERROR,
+    options: {
+      duration: 5_000,
+      horizontalAlignment: ToastHorizontalAlignment.END,
+      verticalAlignment: ToastVerticalAlignment.TOP,
+    },
+    content: 'Something',
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -12,6 +27,7 @@ describe('StandardToastAlertComponent', () => {
 
     fixture = TestBed.createComponent(StandardToastAlertComponent)
     component = fixture.componentInstance
+    component.toast = exampleToast
     fixture.detectChanges()
   })
 
