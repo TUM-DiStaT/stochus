8fa992f28b850d4fc8c135695ac88758c90a8325
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-04-09 17:15:06 +0200

    Declare routes without app UI

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { NxWelcomeComponent } from './nx-welcome.component'
-import { RouterModule } from '@angular/router'
+import {
+  ActivatedRouteSnapshot,
+  NavigationEnd,
+  Router,
+  RouterModule,
+} from '@angular/router'
 import { Component, OnInit } from '@angular/core'
 import { KeycloakService } from 'keycloak-angular'
 import { HttpClient, HttpClientModule } from '@angular/common/http'
-import { Subject } from 'rxjs'
+import { filter, map, Subject } from 'rxjs'
 import { AsyncPipe, JsonPipe } from '@angular/common'
+import { StochusRouteData } from './app.routes'
 
 @Component({
   standalone: true,
@@ -22,10 +28,29 @@ import { AsyncPipe, JsonPipe } from '@angular/common'
 export class AppComponent implements OnInit {
   title = 'frontend'
   hello$ = new Subject()
+  hideAppUi$ = this.router.events.pipe(
+    filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+    map(() => {
+      let route: ActivatedRouteSnapshot | null =
+        this.router.routerState.root.snapshot
+
+      do {
+        const data: StochusRouteData | undefined = route?.data
+        if (data.hideAppUi) {
+          return true
+        }
+
+        route = route?.firstChild ?? null
+      } while (route)
+
+      return false
+    }),
+  )
 
   constructor(
     private readonly keycloak: KeycloakService,
     private readonly http: HttpClient,
+    private readonly router: Router,
   ) {}
 
   ngOnInit() {
