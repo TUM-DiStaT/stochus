ece36503f31e7d0703722e1843e1d6b04d6fdd3f
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-04-09 20:30:55 +0200

    Add toast component

---
+++ libs/daisy-ui/src/lib/toast/toast.component.ts
@@ -0,0 +1,34 @@
+import { Component, HostBinding, Input } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import * as classNames from 'classnames'
+
+export enum ToastHorizontalAlignment {
+  START = 'toast-start',
+  CENTER = 'toast-center',
+  END = 'toast-end',
+}
+
+export enum ToastVerticalAlignment {
+  TOP = 'toast-top',
+  MIDDLE = 'toast-middle',
+  BOTTOM = 'toast-bottom',
+}
+
+@Component({
+  selector: 'daisy-toast',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './toast.component.html',
+})
+export class ToastComponent {
+  @Input()
+  horizontalAlignment: ToastHorizontalAlignment = ToastHorizontalAlignment.END
+
+  @Input()
+  verticalAlignment: ToastVerticalAlignment = ToastVerticalAlignment.TOP
+
+  @HostBinding('class')
+  get class() {
+    return classNames('toast', this.horizontalAlignment, this.verticalAlignment)
+  }
+}
