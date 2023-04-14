02511abaad5834d42e71ced1a87f35d764094498
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-04-14 19:53:34 +0200

    Add menu component

---
+++ libs/daisy-ui/src/lib/menu/menu-item/menu-item.component.ts
@@ -0,0 +1,39 @@
+import { Component, HostBinding, Input } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import * as classNames from 'classnames'
+
+@Component({
+  // Prefix is applied to attribute here instead of
+  // tag name
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'li[daisyMenuItem]',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './menu-item.component.html',
+})
+export class MenuItemComponent {
+  @Input()
+  class?: string
+
+  @Input()
+  active: boolean | '' = false
+
+  @Input()
+  disabled: boolean | '' = false
+
+  @Input()
+  bordered: boolean | '' = false
+
+  @Input()
+  hoverBordered: boolean | '' = false
+
+  @HostBinding('class')
+  get classes() {
+    return classNames(this.class, {
+      disabled: this.disabled !== false,
+      active: this.active !== false,
+      bordered: this.bordered !== false,
+      'hover-bordered': this.hoverBordered !== false,
+    })
+  }
+}
