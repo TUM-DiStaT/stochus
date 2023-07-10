1bf887dc0216883d912493853be51b6e79354ed3
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-07-10 15:04:07 +0200

    Fix tests

--- libs/assignments/core/frontend/src/lib/assignment-completion-process-host/assignment-completion-process-host.component.spec.ts
+++ libs/assignments/core/frontend/src/lib/assignment-completion-process-host/assignment-completion-process-host.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ActivatedRoute } from '@angular/router'
+import 'reflect-metadata'
+import { of } from 'rxjs'
+import { GuessRandomNumberAssignment } from '@stochus/assignments/demos/guess-random-number/shared'
 import { AssignmentCompletionProcessHostComponent } from './assignment-completion-process-host.component'
 
 describe('AssignmentCompletionProcessHostComponent', () => {
@@ -7,7 +12,20 @@ describe('AssignmentCompletionProcessHostComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AssignmentCompletionProcessHostComponent],
+      imports: [
+        AssignmentCompletionProcessHostComponent,
+        HttpClientTestingModule,
+      ],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of({
+              assignmentId: GuessRandomNumberAssignment.id,
+            }),
+          },
+        },
+      ],
     }).compileComponents()
 
     fixture = TestBed.createComponent(AssignmentCompletionProcessHostComponent)
--- libs/assignments/core/frontend/src/lib/assignments-list/assignments-list.component.spec.ts
+++ libs/assignments/core/frontend/src/lib/assignments-list/assignments-list.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ActivatedRoute } from '@angular/router'
+import 'reflect-metadata'
 import { AssignmentsListComponent } from './assignments-list.component'
 
 describe('AssignmentsListComponent', () => {
@@ -7,7 +10,13 @@ describe('AssignmentsListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AssignmentsListComponent],
+      imports: [AssignmentsListComponent, HttpClientTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {},
+        },
+      ],
     }).compileComponents()
 
     fixture = TestBed.createComponent(AssignmentsListComponent)
