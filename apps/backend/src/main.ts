f3d9f54a8f2bde53aa316620bbc66751b8d91e51
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2023-11-01 23:09:29 +0100

    Properly serialize responses

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,14 +2,23 @@
  * This is not a production server yet!
  * This is only a minimal backend to get started.
  */
-import { Logger, ValidationPipe } from '@nestjs/common'
-import { NestFactory } from '@nestjs/core'
+import {
+  ClassSerializerInterceptor,
+  Logger,
+  ValidationPipe,
+} from '@nestjs/common'
+import { NestFactory, Reflector } from '@nestjs/core'
 import { AppModule } from './app/app.module'
 import { HttpLoggingInterceptor } from './app/http-logging.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalInterceptors(new HttpLoggingInterceptor())
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector), {
+      excludeExtraneousValues: true,
+    }),
+  )
   app.useGlobalPipes(
     new ValidationPipe({
       transformOptions: {
