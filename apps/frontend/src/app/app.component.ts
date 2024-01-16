313165a0ebfd7c3f6a329a7b6be9ad1a62d0a7c1
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2024-01-16 17:38:17 +0100

    Display histogram preview in config form

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { AsyncPipe, JsonPipe } from '@angular/common'
 import { HttpClientModule } from '@angular/common/http'
 import { Component } from '@angular/core'
 import { RouterModule } from '@angular/router'
+import { NgChartsConfiguration } from 'ng2-charts'
 import { ToastServiceHostComponent } from '@stochus/daisy-ui'
 import { NavbarComponent } from './navbar/navbar.component'
 
@@ -14,6 +15,14 @@ import { NavbarComponent } from './navbar/navbar.component'
     JsonPipe,
     NavbarComponent,
     ToastServiceHostComponent,
+  ],
+  providers: [
+    {
+      provide: NgChartsConfiguration,
+      useValue: {
+        generateColors: true,
+      } satisfies NgChartsConfiguration,
+    },
   ],
   selector: 'stochus-root',
   templateUrl: './app.component.html',
--- libs/assignments/extract-from-histogram/frontend/src/lib/config-form/extract-from-histogram-config-form.component.ts
+++ libs/assignments/extract-from-histogram/frontend/src/lib/config-form/extract-from-histogram-config-form.component.ts
@@ -1,7 +1,11 @@
-import { JsonPipe } from '@angular/common'
+import { AsyncPipe } from '@angular/common'
 import { Component, Input, OnDestroy } from '@angular/core'
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms'
+import { ChartData, ChartOptions, ChartType } from 'chart.js'
+import DataLabelsPlugin from 'chartjs-plugin-datalabels'
+import { NgChartsModule } from 'ng2-charts'
 import { FormModel } from 'ngx-mf'
+import { Observable, concat, map, of, shareReplay } from 'rxjs'
 import { ExtractFromHistogramAssignmentConfiguration } from '@stochus/assignments/extract-from-histogram-assignment/shared'
 import { AssignmentConfigFormProps } from '@stochus/assignments/model/frontend'
 
@@ -9,7 +13,7 @@ import { AssignmentConfigFormProps } from '@stochus/assignments/model/frontend'
   standalone: true,
   templateUrl: './extract-from-histogram-config-form.component.html',
   styles: [':host { display: contents }'],
-  imports: [ReactiveFormsModule, JsonPipe],
+  imports: [ReactiveFormsModule, NgChartsModule, AsyncPipe],
 })
 export class ExtractFromHistogramConfigFormComponent
   implements
@@ -23,20 +27,30 @@ export class ExtractFromHistogramConfigFormComponent
     formControl: FormModel<ExtractFromHistogramAssignmentConfiguration>,
   ) {
     this._formControl = formControl
-    this.csvFormControl.setValue(formControl.value.data?.join(', ') ?? '')
+    this.csvFormControl.setValue(formControl.value.data?.join(', ') ?? '', {
+      emitEvent: true,
+    })
   }
+
   get formControl() {
     return this._formControl
   }
 
-  csvFormControl = new FormControl<string>('', {
-    validators: [
-      Validators.required,
-      Validators.pattern(/^\s*,?\s*\d+\s*(?:,\s*\d+\s*)*,?\s*$/),
-    ],
-  })
-  csvTransformationSubscription = this.csvFormControl.valueChanges.subscribe(
-    (value) => {
+  csvFormControl = new FormControl<string>(
+    this.formControl?.controls.data?.value?.join(', ') ?? '',
+    {
+      validators: [
+        Validators.required,
+        Validators.pattern(/^\s*,?\s*\d+\s*(?:,\s*\d+\s*)*,?\s*$/),
+      ],
+    },
+  )
+  parsedCsv$ = concat(
+    of(this.formControl?.controls.data?.value?.join(', ') ?? ''),
+    this.csvFormControl.valueChanges,
+  ).pipe(
+    shareReplay(),
+    map((value) => {
       let parsedCsv: number[] | undefined = value
         ?.split(/\s*,\s*/gim)
         .filter(Boolean)
@@ -51,14 +65,57 @@ export class ExtractFromHistogramConfigFormComponent
         parsedCsv = undefined
       }
 
-      this.formControl.patchValue(
-        {
-          data: parsedCsv,
-        },
-        {},
-      )
-    },
+      return parsedCsv
+    }),
   )
+  csvTransformationSubscription = this.parsedCsv$.subscribe((data) => {
+    this.formControl?.patchValue(
+      {
+        data,
+      },
+      {},
+    )
+  })
+
+  chartType = 'bar' as const satisfies ChartType
+  chartOptions: ChartOptions<typeof this.chartType> = {
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {},
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+      datalabels: {
+        anchor: 'end',
+        align: 'end',
+      },
+    },
+  }
+  chartPlugins = [DataLabelsPlugin]
+  chartData$: Observable<ChartData<typeof this.chartType>> =
+    this.parsedCsv$.pipe(
+      map((data) => {
+        // compute frequency of each item in data
+        const frequencies = new Map<number, number>()
+        for (const item of data ?? []) {
+          frequencies.set(item, (frequencies.get(item) ?? 0) + 1)
+        }
+        const frequencyEntries = [...frequencies.entries()].sort(
+          ([a], [b]) => a - b,
+        )
+        return {
+          labels: frequencyEntries.map(([value]) => value.toString()),
+          datasets: [
+            {
+              data: frequencyEntries.map(([, frequency]) => frequency),
+            },
+          ],
+        }
+      }),
+    )
 
   ngOnDestroy() {
     this.csvTransformationSubscription.unsubscribe()
