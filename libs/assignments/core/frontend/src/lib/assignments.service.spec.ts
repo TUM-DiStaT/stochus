d61d999e5773697bcb6fc29bda0d5ef70834749c
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2024-01-18 02:37:42 +0100

    Fix tests

--- libs/assignments/core/frontend/src/lib/assignments.service.spec.ts
+++ libs/assignments/core/frontend/src/lib/assignments.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing'
+import 'reflect-metadata'
 import { AssignmentsService } from './assignments.service'
 
 describe('AssignmentsService', () => {
--- libs/identify-shared-characteristics/shared/src/lib/generate-datasets.spec.ts
+++ libs/identify-shared-characteristics/shared/src/lib/generate-datasets.spec.ts
@@ -5,14 +5,14 @@ import {
 } from './generate-datasets'
 
 it('sanity check: means should be identical', () => {
-  const datasets = generateDatasetsWithIdenticalMean()
+  const datasets = generateDatasetsWithIdenticalMean(4)
   const [first, ...rest] = datasets.map((d) => calculateMean(d))
   const expected = Array.from({ length: rest.length }, () => first)
   expect(rest).toEqual(expected)
 })
 
 it('sanity check: medians should be identical', () => {
-  const datasets = generateDatasetsWithIdenticalMedian()
+  const datasets = generateDatasetsWithIdenticalMedian(4)
   const [first, ...rest] = datasets.map((d) => calculateMedian(d))
   const expected = Array.from({ length: rest.length }, () => first)
   expect(rest).toEqual(expected)
