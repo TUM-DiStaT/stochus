661600abb66201518b4f105623789e62865d312b
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2024-02-18 02:24:57 +0100

    Adjust dice size dynamically

--- libs/core/frontend/src/lib/animated-dice/animated-dice.component.ts
+++ libs/core/frontend/src/lib/animated-dice/animated-dice.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, Input } from '@angular/core'
 import { random } from 'lodash'
 import { BehaviorSubject } from 'rxjs'
 import { DieComponent } from '../die/die.component'
@@ -9,16 +9,32 @@ import { DieComponent } from '../die/die.component'
   templateUrl: './animated-dice.component.html',
   imports: [DieComponent],
 })
-export class AnimatedDiceComponent implements OnInit {
+export class AnimatedDiceComponent {
+  @Input()
+  set count(count: number) {
+    this.values = Array.from({ length: count }, () => new BehaviorSubject(1))
+  }
+
+  get count() {
+    return this.values.length
+  }
+
   values = Array.from({ length: 10 }, () => new BehaviorSubject(1))
+
+  canRoll = true
 
   roll() {
     for (const value of this.values) {
       value.next(random(1, 6))
     }
+
+    this.canRoll = false
+    setTimeout(() => {
+      this.canRoll = true
+    }, 1_200)
   }
 
-  ngOnInit() {
-    this.roll()
+  get diceSize() {
+    return 100 / Math.log(0.6 * (this.values.length + 1))
   }
 }
