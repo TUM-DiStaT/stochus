5d00a8a47200a44649da770e82ff6d2caaa38504
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2024-02-26 16:42:26 +0100

    Fix dataset generation

--- libs/assignments/identify-shared-characteristics/shared/src/lib/generate-datasets.spec.ts
+++ libs/assignments/identify-shared-characteristics/shared/src/lib/generate-datasets.spec.ts
@@ -5,14 +5,14 @@ import {
 } from './generate-datasets'
 
 it('sanity check: means should be identical', () => {
-  const datasets = generateDatasetsWithIdenticalMean(4)
+  const datasets = generateDatasetsWithIdenticalMean(4, 20, 50)
   const [first, ...rest] = datasets.map((d) => calculateMean(d))
   const expected = Array.from({ length: rest.length }, () => first)
   expect(rest).toEqual(expected)
 })
 
 it('sanity check: medians should be identical', () => {
-  const datasets = generateDatasetsWithIdenticalMedian(4)
+  const datasets = generateDatasetsWithIdenticalMedian(4, 20, 50)
   const [first, ...rest] = datasets.map((d) => calculateMedian(d))
   const expected = Array.from({ length: rest.length }, () => first)
   expect(rest).toEqual(expected)
