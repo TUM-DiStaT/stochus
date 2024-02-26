659b95c1c7e051e1174eec96c21221716635dd26
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2024-02-26 09:34:50 +0100

    Fix frontend service generation

--- libs/nx-plugin/plugin/src/generators/assignment/generator.ts
+++ libs/nx-plugin/plugin/src/generators/assignment/generator.ts
@@ -14,40 +14,6 @@ import {
 } from './file-paths'
 import { AssignmentGeneratorSchema } from './schema'
 
-function addToFrontendService(
-  tree: Tree,
-  camelCasedName: string,
-  options: AssignmentGeneratorSchema,
-) {
-  const frontendAssignmentService =
-    tree.read(frontendAssignmentsServicePath)?.toString() ?? ''
-  const withImport = tsquery.replace(
-    frontendAssignmentService,
-    // last-child doesn't seem to work and the order will be fixed by prettier
-    'ImportDeclaration:first-child',
-    (node) => {
-      return (
-        node.getText() +
-        '\n' +
-        `import { ${camelCasedName}AssignmentForFrontend } from '@stochus/assignments/${options.name}/frontend'`
-      )
-    },
-  )
-  const withAddedAssignment = tsquery.replace(
-    withImport,
-    'ClassDeclaration:has(Identifier[name="AssignmentsService"]) > PropertyDeclaration:has(Identifier[name="assignments"]) > ArrayLiteralExpression AsExpression:last-child',
-    (node) => {
-      return (
-        node.getText() +
-        `,
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        ${camelCasedName}AssignmentForFrontend as any`
-      )
-    },
-  )
-  tree.write(frontendAssignmentsServicePath, withAddedAssignment)
-}
-
 const addImport = (original: string, newImport: string): string => {
   const nodes = tsquery.query(
     original,
@@ -62,6 +28,45 @@ const addImport = (original: string, newImport: string): string => {
   const node = nodes[0]
 
   return original.replace(node.getText(), node.getText() + '\n' + newImport)
+}
+
+function addToFrontendService(
+  tree: Tree,
+  camelCasedName: string,
+  options: AssignmentGeneratorSchema,
+) {
+  const frontendAssignmentService =
+    tree.read(frontendAssignmentsServicePath)?.toString() ?? ''
+  const withImport = addImport(
+    frontendAssignmentService,
+    `import { ${camelCasedName}AssignmentForFrontend } from '@stochus/assignments/${options.name}/frontend'`,
+  )
+
+  const assignmentsArrayNode = tsquery.query(
+    withImport,
+    'ClassDeclaration:has(Identifier[name="AssignmentsService"]) > PropertyDeclaration:has(Identifier[name="assignments"]) > ArrayLiteralExpression',
+  )
+  const lastAssignmentNode = tsquery.query(
+    withImport,
+    'ClassDeclaration:has(Identifier[name="AssignmentsService"]) > PropertyDeclaration:has(Identifier[name="assignments"]) > ArrayLiteralExpression AsExpression:last-child',
+  )
+
+  if (assignmentsArrayNode.length !== 1 || lastAssignmentNode.length !== 1) {
+    console.error(withImport)
+    throw new Error('Could not find the assignments array node')
+  }
+
+  const withAddedAssignment = withImport.replace(
+    assignmentsArrayNode[0].getText(),
+    assignmentsArrayNode[0].getText().replace(
+      lastAssignmentNode[0].getText(),
+      `${lastAssignmentNode[0].getText()},
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ${camelCasedName}AssignmentForFrontend as any`,
+    ),
+  )
+
+  tree.write(frontendAssignmentsServicePath, withAddedAssignment)
 }
 
 async function addToBackendService(
